2e3ddc803290beb9c5b42d8753da32d5e6c8708a
tehrimabbas <syedtehrimabbas@gmail.com>
2020-05-11 23:54:46 +0500

    Biodata Form Design Cange

---
+++ src/theme/colors.js
@@ -0,0 +1,22 @@
+const colors = {
+    white: '#fff',
+    black: '#000',
+    white_light: '#FFFFFF',
+    white_dark: '#f1f1f1',
+    white_most_dark: '#f1f1f1',
+    grey: '#C0C0C0',
+    grey1: '#616161',
+    lightgrey: "#F4F4F4",
+    dark_grey: '#616161',
+    greyTabs: "#E1DEE5",
+    purple: '#432370',
+    dark_purple: '#0e0022',
+    // dark_purple: '#0c001e',
+    gold: '#ffa800',
+    blue: '#1a72d6',
+    pink: '#de0044',
+    cyan: '#12b191'
+    // your colors
+}
+
+export default colors;
